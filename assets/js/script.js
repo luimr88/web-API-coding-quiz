ebf033bc930f998286f0c471058903dbb3afcde4
Luis <luisriveros26@gmail.com>
2022-04-09 22:33:00 -0600

    added switching question function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,73 @@
 var startBtn = document.getElementById('start-game-btn');
+var gameIntro = document.getElementById('game-start');
+var gameContainer = document.getElementById('game-container');
+var quizQuestions = document.getElementById('question');
+var choiceArea = document.getElementById('choice-area');
+var buttons = document.getElementById('btn');
+
+var index = 0;
+var questions = [
+    {
+        title: "What is 2 + 2?",
+        choices:["1", "2", "3", "4"],
+        answer: "4"
+    },
+    {
+        title: "What is 5 + 5?",
+        choices:["1", "2", "10", "4"],
+        answer: "10"
+    }
+
+];
+
+function displayQuestions() {
+    //var question = ' ';
+    quizQuestions.innerHTML = "";
+    choiceArea.innerHTML = "";
+    var questionTitle = questions[index].title;
+    var choiceButtons = questions[index].choices;
+    quizQuestions.append(questionTitle);
+
+    // var choiceSection = document.createElement("div");
+    // choiceSection.setAttribute("class", "choice-btns");
+    // choiceSection.setAttribute("id", "choice-area");
+    // choiceArea.appendChild(choiceSection);
+    
+    for(var i = 0; i < choiceButtons.length; i++) {
+        var button = document.createElement('button');
+        button.classList.add('btn');
+        button.textContent = choiceButtons[i];
+
+        if (choiceButtons[i] === questions[index].answer) {
+            button.setAttribute("data-value", "true");
+        } else {
+            button.setAttribute("data-value", "false");
+        }
+        choiceArea.appendChild(button);
+        button.addEventListener("click", checkAnswer);
+    }
+
+}
+
+function checkAnswer(event) {
+    var buttonId = event.currentTarget.dataset.value;
+    if(buttonId === "true") {
+        index++
+        displayQuestions();
+        console.log(buttonId);
+    } else {
+        index++
+        displayQuestions();
+        console.log(buttonId);
+    }
+}
+
 var displayTimer = function () {
-    var timerStart = 59;
+    //gameStart();
+    var timerStart = 60;
     var timer = document.getElementById('timer');
     var countdown = setInterval(function () {
-        console.log(timerStart);
+        //console.log(timerStart);
         timer.textContent = 'Timer: ' + timerStart;
         timerStart--;
         if (timerStart === -1) {
@@ -17,8 +81,24 @@ var displayTimer = function () {
     }
 }
 
+function gameQuestions() {
+    gameContainer.classList.add("hide");
+}
 
 
-displayTimer();
 
-startBtn.addEventListener("click", displayTimer());
+function gameStart() {
+    gameIntro.classList.add("hide");
+    displayTimer();
+    gameContainer.classList.remove("hide");
+    displayQuestions();
+}
+
+gameQuestions();
+
+
+
+
+
+
+startBtn.addEventListener("click", gameStart);
