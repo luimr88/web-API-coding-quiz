e06f317906f886c6b078170c916baddfe6cd460e
Luis <luisriveros26@gmail.com>
2022-04-10 18:43:47 -0600

    updated js

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,17 @@ var gameContainer = document.getElementById('game-container');
 var quizQuestions = document.getElementById('question');
 var choiceArea = document.getElementById('choice-area');
 var buttons = document.getElementById('btn');
+var gameEnd = document.getElementById('form-area');
+var timer = document.getElementById('timer');
+var playerForm = document.getElementById('initials-input');
+var scoreDisplay = document.getElementById('score');
 
+var countdown;
+var timerStart = 60;
 var index = 0;
+var score = 0;
+var name = [];
+var scoreStorage = JSON.parse(localStorage.getItem("scoreStorage")) || [];
 var questions = [
     {
         title: "What is 2 + 2?",
@@ -52,43 +61,95 @@ function displayQuestions() {
 function checkAnswer(event) {
     var buttonId = event.currentTarget.dataset.value;
     if(buttonId === "true") {
-        index++
-        displayQuestions();
+        index++;
+        score+=5;
+        console.log(score);
         console.log(buttonId);
-    } else {
+        if(index === questions.length){
+            gameOver();
+            resetTimer();
+        } else {
+            displayQuestions();
+        }
+    } else if(buttonId === "false") {
         index++
-        displayQuestions();
+        subtractTime();
         console.log(buttonId);
+        if(index === questions.length){
+            gameOver();
+            resetTimer();
+        } else {
+            displayQuestions();
+        }
     }
 }
 
 var displayTimer = function () {
     //gameStart();
-    var timerStart = 60;
-    var timer = document.getElementById('timer');
-    var countdown = setInterval(function () {
+    //var timer = document.getElementById('timer');
+    countdown = setInterval(function () {
         //console.log(timerStart);
         timer.textContent = 'Timer: ' + timerStart;
         timerStart--;
-        if (timerStart === -1) {
-            stopInterval()
+        if (timerStart <= 0) {
+            stopInterval();
+            gameOver();
+            //resetTimer();
         }
     }, 1000);
 
     var stopInterval = function () {
         console.log('time is up!');
         clearInterval(countdown);
+        resetTimer();
     }
+}
+
+function resetTimer() {
+    clearInterval(countdown);
+    timerStart = 60;
+    timer.textContent = 'Timer: ' + timerStart;
+}
+
+function subtractTime() {
+    timerStart = timerStart - 5;
+    if(timerStart <= 0) {
+        clearInterval(countdown);
+        timerStart = 60;
+        timer.textContent = 'Timer: ' + timerStart;
+        gameOver();
+    }
+}
+
+function playerNameInput (event) {
+    event.preventDefault();
+    var nameInput = document.querySelector("input[name='name-input']").value;
+    var scoreInput = localStorage.getItem("mostRecentScore");
+    var scoreTable = {
+        initials: nameInput,
+        score: scoreInput
+    };
+    scoreStorage.push(scoreTable);
+    localStorage.setItem("scoreStorage", JSON.stringify(scoreStorage));
+    console.log(scoreStorage);
 }
 
 function gameQuestions() {
     gameContainer.classList.add("hide");
+    gameEnd.classList.add("hide");
+
 }
 
-
+function gameOver() {
+    gameEnd.classList.remove("hide");
+    gameContainer.classList.add("hide");
+    localStorage.setItem('mostRecentScore', score);
+    scoreDisplay.textContent = "Score: " + score;
+}
 
 function gameStart() {
     gameIntro.classList.add("hide");
+    //gameEnd.classList.add("hide");
     displayTimer();
     gameContainer.classList.remove("hide");
     displayQuestions();
@@ -96,9 +157,5 @@ function gameStart() {
 
 gameQuestions();
 
-
-
-
-
-
 startBtn.addEventListener("click", gameStart);
+playerForm.addEventListener("submit", playerNameInput)
